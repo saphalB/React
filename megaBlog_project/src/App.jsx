6d77c0b9fda2f808cfc9ededbf5b331382e4695c
Saphal Bhattarai <saphalbhattarai5@gmail.com>
2024-12-09 08:25:43 +0000

    megaProject onTheWay

---
+++ megaBlog_project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+
+  return (
+   <>
+   <h1>A blog app  project with appwrite</h1>
+   </>
+  )
+}
+
+export default App
